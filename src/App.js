e0a4a7f3626733228d2605a8db5f8f793396991c
Dontha Suman <51446173+SumanDontha16@users.noreply.github.com>
2021-12-10 11:43:18 +0000

    Api Calls with Hooks.

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import commentsApi from "./api/comments";
+import postsApi from "./api/posts";
+import useApi from "./hooks/useApi";
+
+function App() {
+	const getPostsApi = useApi(postsApi.getPosts);
+	const getCommentsApi = useApi(commentsApi.getComments);
+
+	useEffect(() => {
+		getPostsApi.request();
+		getCommentsApi.request();
+	}, []);
+
+	return (
+		<div className="App">
+			{/* Posts */}
+			<div>
+				<h1>Posts</h1>
+				{getPostsApi.loading && <p>Posts are loading!</p>}
+				{getPostsApi.error && <p>{getPostsApi.error}</p>}
+				<ul>
+					{getPostsApi.data?.map((post) => (
+						<li key={post.id}>{post.title}</li>
+					))}
+				</ul>
+			</div>
+			{/* Comment List */}
+			<div>
+				<h1>Comments</h1>
+				{getCommentsApi.loading && <p>Comments are loading!</p>}
+				{getCommentsApi.error && <p>{getCommentsApi.error}</p>}
+				<ul>
+					{getCommentsApi.data?.map((comment) => (
+						<li key={comment.id}>{comment.name}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default App;
